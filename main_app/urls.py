bae07566d36261399713d1863a2dc62e0833c5a0
aeshah alalwani <aisha.r.alalwani@gmail.com>
2025-11-01 15:24:56 +0300

    add improved setting

--- main_app/urls.py
+++ main_app/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignupUserView.as_view(), name='signup'),
-    
     path('logout/', LogoutView.as_view(), name='logout'),
     
 ]
